d0ad05f105973a01504d71055a9632e8067aa6b3
srinandan <nandanks@gmail.com>
2020-10-23 12:59:22 -0700

    err handling

--- setup-loadbalancer.sh
+++ setup-loadbalancer.sh
@@ -58,6 +58,11 @@ echo "Configure a health-check\n"
 gcloud compute health-checks create https hc-apigee-443 \
     --project $project --port 443 --global \
     --request-path /healthz/ingress
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+  echo "failed to create health check"
+  exit 1
+fi
 
 echo "Create backend service\n"
 # Create Backend Service
@@ -65,6 +70,12 @@ gcloud compute backend-services create $backend_name \
     --project $project --protocol HTTPS \
     --health-checks hc-apigee-443 --port-name https \
     --timeout 60s --connection-draining-timeout 300s --global
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+  echo "failed to create backend service"
+  exit 1
+fi
+
 
 echo "Add instance group to backend service\n"
 # Add Instance Group to Backend Service
@@ -72,21 +83,43 @@ gcloud compute backend-services add-backend $backend_name \
     --project $project --instance-group $mig_name \
     --instance-group-region $region \
     --balancing-mode UTILIZATION --max-utilization 0.8 --global
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+  echo "failed to add backend service"
+  exit 1
+fi
 
 echo "Create LB URL Map\n"
 # Create Load Balancing URL Map
 gcloud compute url-maps create apigee-proxy-map \
     --project $project --default-service $backend_name
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+  echo "failed to create url map"
+  exit 1
+fi
+
 
 echo "Create LB target https proxy\n"
 # Create Load Balancing Target HTTPS Proxy
 gcloud compute target-https-proxies create apigee-https-proxy \
     --project $project --url-map apigee-proxy-map \
     --ssl-certificates apigee-ssl-cert
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+  echo "failed to create target proxy"
+  exit 1
+fi
 
+echo "Create forwarding rule\n"
 # Create Global Forwarding Rule
 gcloud compute forwarding-rules create apigee-https-lb-rule \
     --project $project --address lb-ipv4-vip-1 --global \
     --target-https-proxy apigee-https-proxy --ports 443
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+  echo "failed to add forwarding rule"
+  exit 1
+fi    
 
 echo "Load balancer complete. Try api as curl https://$lb_ip -kv\n"
